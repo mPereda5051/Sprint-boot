81240be063245d90adcd63515bf89686cc6ab5a4
mPereda5051 <98544413+mPereda5051@users.noreply.github.com>
2026-01-19 22:42:02 +0100

    creado projecto maven

---
+++ src/test/java/com/nagisa/prueba_springBoot/PruebaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagisa.prueba_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
